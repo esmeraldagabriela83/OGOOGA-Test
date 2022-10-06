c7c3aa7d2b0783ac8c6dfbee0df1daf581796877
Acornicesei Mihaela Gabriela <esmeraldagabriela83@yahoo.com>
2022-10-06 12:46:39 +0300

    last day project casino sections

--- js/ogooga.js
+++ js/ogooga.js
@@ -1,1 +1,35 @@
 console.log("Hello from ogooga.js file");
+
+const allBtn=document.querySelectorAll('.btn');
+console.log("arr allBtn is : " , allBtn);
+console.log(`from ogooga.js , landing page casino has ${allBtn.length}  buttons : ` , allBtn.length);
+
+//landing page casino has 28  buttons :  28
+
+
+for(let i=0 ; i<allBtn.length ; i++){
+  allBtn[i].addEventListener("click" , function(event){
+   event.preventDefault();
+    console.log("you clicked the button");
+
+    console.log(`you clicked the ${i + 1} button`);
+  })
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
